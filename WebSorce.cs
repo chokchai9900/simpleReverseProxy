9e3e7436f28d542fc7ed20b68cf7a9d40c56cbcb
chokchai9900 <48384299+chokchai9900@users.noreply.github.com>
2019-09-26 14:11:46 +0700

    Create WebSorce.cs

---
+++ WebSorce.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpleReverseProxy
+{
+    public class WebSorce
+    {
+        public string youtube { get; set; }
+        public string pornhub { get; set; }
+        public string github { get; set; }
+    }
+}
